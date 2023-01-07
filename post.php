3aab68ac3f06b4752503d3217e0403c4757b5ea6
Mr.pudidi <120796780+Mrfamou@users.noreply.github.com>
2023-01-07 19:26:45 +0700

    Create post.php

---
+++ post.php
@@ -0,0 +1,28 @@
+<?php
+
+    $conn= mysql_connect("b8_32431637_server","Admin","rifki126@");
+      $db= mysql_select_db("abc",$conn);
+      
+//creAT CONECTION
+$connection = new mysqli($conn,$db);
+  
+  //check koneksi
+  if ($connection->connect_error){
+    die("connection failed: ".$connection->connect_error);
+  }
+//read all row fromdatabase table
+$sql = "SELECT * FROM `abc` WHERE 1";
+$result = connection->query($sql);
+
+//raech data of each row
+while($row =$result->fetch_assoc()) {
+}
+      
+      
+      
+     $name=     $_POST['Name'];
+     $name=     $_POST[Email];
+     $name=     $_POST[Pesan];
+
+
+?>
